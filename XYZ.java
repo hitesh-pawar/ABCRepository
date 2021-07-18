ca0ae533e17cc21e5be66aca81265468c8c648a2
hitesh <tech.hiteshpawar@gmail.com>
2021-07-18 10:42:56 +0530

    ABC.java chnages

---
+++ XYZ.java
@@ -0,0 +1,11 @@
+class XYX{
+	public static void main(String args[]){
+		System.out.println("Hello Good Morning");
+	}
+}
+
+class ABC{
+
+	XYX obj1 = new XYX();
+	obj1.main();
+}
---
+++ pppp/src/pppp/TTTTT.java
@@ -0,0 +1,9 @@
+package pppp;
+
+public class TTTTT {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello God Morning");
+	}
+	
+}
